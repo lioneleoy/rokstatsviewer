5d0f4129a7bd1bdd7c0a503ee64efd6e757302a4
Lionel Raja <lioneleoy@gmail.com>
2025-01-19 22:23:02 -0500

    updated data

--- app.py
+++ app.py
@@ -146,7 +146,7 @@ if folder_path:
                                     # Adjust the size of the graph area
                                     chart = chart.properties(
                                         title=f"Trend of {column} over Time for governorID {selected_display}",
-                                        width=1000,  # Increased width
+                                        width=600,  # Increased width
                                         height=600  # Increased height
                                     )
 
